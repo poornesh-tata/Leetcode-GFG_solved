3998899a3510cfcba4089648f5e797a4d539352d
Poornesh Tata <80261388+poornesh-tata@users.noreply.github.com>
2022-10-10 11:25:02 +0530

    Time: 643 ms (13.12%), Space: 81 MB (22.08%) - LeetHub

---
+++ 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int wc(string s)
+    {
+        int c=0;
+        for(auto it:s)
+        {
+            if(it==' ') c++;
+        }
+        return c+1;
+    }
+    string largestWordCount(vector<string>& messages, vector<string>& senders) {
+        unordered_map<string,int> hash;
+        for(int i=0;i<senders.size();i++)
+        {
+            hash[senders[i]]+=wc(messages[i]);
+        }
+        int max=INT_MIN;
+        string ans;
+        for(auto it:hash)
+        {
+            if(it.second>max)
+            {
+                max=it.second;
+            }
+        }
+        vector<string> v;
+        for(auto it:hash) if(it.second==max) v.push_back(it.first);
+        sort(v.begin(),v.end());
+        return v[v.size()-1];
+    }
+};
