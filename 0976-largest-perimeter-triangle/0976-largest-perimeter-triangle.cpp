83502f0ad3db9a1eaaf98835c49a184b5859d2e2
Poornesh Tata <80261388+poornesh-tata@users.noreply.github.com>
2022-10-12 11:28:33 +0530

    Update 0976-largest-perimeter-triangle.cpp

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int largestPerimeter(vector<int>& nums) {
+        // it is known that if size of two sides a+b greater than the third size c then the triangle exists.
+        // Hence we sort the array to get the possible largest perimeter by traversing it from the last.
         sort(nums.begin(),nums.end());
         for(int i=nums.size()-3;i>=0;i--)
         {
