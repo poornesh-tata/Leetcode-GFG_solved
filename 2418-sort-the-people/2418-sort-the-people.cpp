f69d64d48807e920e6e9ebc9cb3eaefb5d93e65b
Poornesh Tata <80261388+poornesh-tata@users.noreply.github.com>
2022-10-14 10:22:28 +0530

    Time: 36 ms (93.57%), Space: 23.5 MB (33.53%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.cpp
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
-    static bool cmp(pair<int,string>a,pair<int,string> b)
-    {
-        return a.first>b.first;
-    }
+    
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
         vector<string> ans;
         vector<pair<int,string>> v;
@@ -11,7 +8,7 @@ public:
         {
             v.push_back({heights[i],names[i]});
         }
-        sort(v.begin(),v.end(),cmp);
+        sort(v.rbegin(),v.rend());
         for(auto it:v) ans.push_back(it.second);
         return ans;
     }
