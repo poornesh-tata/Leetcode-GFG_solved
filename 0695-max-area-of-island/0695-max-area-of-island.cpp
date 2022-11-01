684e758c3dd7e247badf3b4e4667cc057b4a882b
Poornesh Tata <80261388+poornesh-tata@users.noreply.github.com>
2022-11-01 14:22:24 +0530

    Time: 23 ms (88.10%), Space: 27.5 MB (9.15%) - LeetHub

---
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    void bfs(vector<vector<int>>& grid,vector<vector<int>>&vis,int row,int col,int &max)
+    {
+        int count=0;
+        int n=grid.size();
+        int m=grid[0].size();
+        queue<pair<int,int>> q;
+        q.push({row,col});
+        vis[row][col]=1;
+        while(!q.empty())
+        {
+            int nrow=q.front().first;
+            int ncol=q.front().second;
+            q.pop();
+            count++;
+            for(int i=-1;i<=1;i++)
+            {
+                if(i==0) continue;
+                if(ncol+i>=0 and ncol+i<m and grid[nrow][ncol+i]==1 and vis[nrow][ncol+i]==0)
+                {
+                    q.push({nrow,ncol+i});
+                    vis[nrow][ncol+i]=1;
+                }
+                if(nrow+i>=0 and nrow+i<n and grid[nrow+i][ncol]==1 and vis[nrow+i][ncol]==0)
+                {
+                    q.push({nrow+i,ncol});
+                    vis[nrow+i][ncol]=1;
+                }
+            }
+        }
+        if(count>max) max=count;
+    }
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        int count=0;
+        int max=0;
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(vis[i][j]==0 and grid[i][j]==1)
+                {
+                    bfs(grid,vis,i,j,max);
+                }
+            }
+        }
+        return max;
+    }
+};
