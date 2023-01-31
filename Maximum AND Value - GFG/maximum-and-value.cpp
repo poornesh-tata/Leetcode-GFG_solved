d5c4f9851771d0cbfb4db1a878c1a878fb7ed84f
Poornesh Tata <80261388+poornesh-tata@users.noreply.github.com>
2023-01-31 17:21:17 +0530

    Added solution - LeetHub

---
+++ Maximum AND Value - GFG/maximum-and-value.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+ 
+class Solution
+{
+    public:
+    // Function for finding maximum AND value.
+    int maxAND (int a[], int n)
+    {
+        // Your code here
+        int res=0;
+        for(int i=31;i>=0;i--)
+        {
+            int check = res | (1<<i);
+            int count=0;
+            for(int j=0;j<n;j++)
+            {
+                if( (a[j]&check)==check)
+                {
+                    count++;
+                }
+            }
+            if(count>1)
+            {
+                res=res|(1<<i);
+            }
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+ 
+// Driver function
+int main()
+{
+    int t;
+    cin>>t;//testcases
+    while(t--)
+    {
+        int n;
+        cin>>n;//input n
+        int arr[n+5],i;
+        
+        //inserting elements
+        for(i=0;i<n;i++)
+        cin>>arr[i];
+        Solution obj;
+        //calling maxAND() function
+        cout <<  obj.maxAND(arr,n)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
