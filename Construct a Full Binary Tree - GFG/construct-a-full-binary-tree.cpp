fdb8b32bb4910f3ea40c603f6fdc549bdbc53501
Poornesh Tata <80261388+poornesh-tata@users.noreply.github.com>
2023-05-23 15:46:11 +0530

    Added solution - LeetHub

--- Construct a Full Binary Tree - GFG/construct-a-full-binary-tree.cpp
+++ Construct a Full Binary Tree - GFG/construct-a-full-binary-tree.cpp
@@ -17,39 +17,6 @@ struct Node
 };
 
 
-// } Driver Code Ends
-/* Structre of the Node of the Binary Tree is as follows
-struct Node
-{
-        int data;
-        struct Node *left, *right;
-};
-*/
-// your task is to complete this function
-// function should create a new binary tree
-// function should return the root node to the 
-// new binary tree formed
-class Solution{
-  public:
-    Node* fun(int l,int r,int pre[])
-    {
-        int mid=(l+r)/2;
-        Node* root=new Node(pre[l]);
-        if(l<r)
-        {
-            root->left=fun(l+1,mid,pre);
-            root->right=fun(mid+1,r,pre);
-        }
-        return root;
-    }
-    Node* constructBinaryTree(int pre[], int preMirror[], int size)
-    {
-        return fun(0,size-1,pre);
-    }
-};
-
-//{ Driver Code Starts.
-
 void printInorder(Node* node)
 {
 	if (node == NULL)return;
@@ -57,6 +24,8 @@ void printInorder(Node* node)
 	cout<<node->data<<" ";
 	printInorder(node->right);
 }
+
+Node* constructBinaryTree(int pre[], int preMirror[], int size);
 
 // Driver program to test above functions
 int main()
@@ -71,10 +40,37 @@ int main()
 	    int preOrderMirror[n];
 	    for(int i=0; i<n; i++)cin>>preOrder[i];
 	    for(int i=0; i<n; i++)cin>>preOrderMirror[i];
-	    Solution obj;
-	    printInorder(obj.constructBinaryTree(preOrder, preOrderMirror, n));
+	    printInorder(constructBinaryTree(preOrder, preOrderMirror, n));
 	    cout<<endl;
     }
 	return 0;
 }
 // } Driver Code Ends
+
+
+/* Structre of the Node of the Binary Tree is as follows
+struct Node
+{
+        int data;
+        struct Node *left, *right;
+};
+*/
+// your task is to complete this function
+// function should create a new binary tree
+// function should return the root node to the 
+// new binary tree formed
+Node* fun(int l,int r,int pre[])
+{
+    int mid=(l+r)/2;
+    Node* root=new Node(pre[l]);
+    if(l<r)
+    {
+        root->left=fun(l+1,mid,pre);
+        root->right=fun(mid+1,r,pre);
+    }
+    return root;
+}
+Node* constructBinaryTree(int pre[], int preMirror[], int size)
+{
+    return fun(0,size-1,pre);
+}
